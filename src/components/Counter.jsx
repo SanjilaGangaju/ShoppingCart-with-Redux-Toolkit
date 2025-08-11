1aee627be6c630579da675dbcb191e21538acfb9
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-08-11 21:33:12 +0545

    create counter by redux

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { increaseby1 , decreaseby1} from '../redux/counterSlice';
 const Counter = () => {
      const count = useSelector((state)=>state.counter)
+     const dispatch = useDispatch();
   return (
     <div>
         <h1>{count}</h1>
+        <button onClick={()=>dispatch(increaseby1())}>Increase By 1</button>
+        <button onClick={()=>dispatch(decreaseby1())}>Decrease By 1</button>
     </div>
   )
 }
